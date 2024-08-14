24de3bbbb41356588f8f8f8a17b8cc4906f6f5be
Muhammad-Rizki-Putra <muhammad22517@mail.unpad.ac.id>
2024-08-15 03:33:00 +0700

    premiere lauch/go page

--- resources/views/events/grand-opening.blade.php
+++ resources/views/events/grand-opening.blade.php
@@ -1,4 +1,43 @@
 <x-layout>
 <x-slot:pagename>{{$pagename}}</x-slot:pagename>
-    <h3>Grand Opening</h3>
+    <div class="flex items-center justify-center flex-col h-auto bg-home-bg bg-cover bg-no-repeat bg-center py-20 gap-y-32">
+        <h1 class="text-7xl font-bold">PREMIERE LAUNCH</h1>
+
+        <x-countdown>2024-08-25</x-countdown>
+
+        <div class="bg-white bg-opacity-60 rounded-xl shadow-lg p-6 w-full max-w-lg">
+            <p class="text-center text-gray-800 text-lg font-medium">The IFest 2024 Premiere Launch is a series of opening events featuring an online talk show concept. This activity aims to serve as a platform that enables Founders or Strategic Partners from several companies operating in the Artificial Intelligence and Financial Technology sectors, as well as experts and specialists in those fields, to meet with participants and share experiences and knowledge related to the industry.</p>
+        </div>
+
+        <div class="bg-navy-bg rounded-xl shadow-lg p-6 w-full max-w-lg">
+            <p class="text-center text-white text-3xl font-medium">Sunday, August 25th 2024!</p>
+        </div>
+
+        <div>
+            <div class="bg-transparent p-6 w-full max-w-lg">
+                <p class="text-center text-black text-3xl font-medium">To receive more details, please keep an eye on our Instagram!</p>
+            </div>
+
+            <a href="https://www.instagram.com/ifestunpad/" class="w-full max-w-sm">
+                <div class="bg-transparent rounded-xl p-6 px-2 w-full max-w-lg flex flex-row items-center justify-center gap-x-3 outline outline-2 outline-offset-2 outline-navy-bg">
+                    <img src="/image/Instagram.png" alt="">
+                    <p class="text-center text-navy-bg text-3xl font-medium">@ifest.unpad</p>
+                </div>
+            </a>
+        </div>
+
+        <div>
+            <div class="bg-transparent p-6 w-full max-w-lg">
+                <p class="text-center text-black text-3xl font-medium">Will be held via Online Meeting</p>
+            </div>
+
+            <a href="https://www.gatau.com" class="w-full max-w-lg">
+                <div class="bg-transparent rounded-xl w-full max-w-lg flex flex-row items-center justify-center gap-x-3 outline outline-2 outline-offset-2 outline-navy-bg">
+                    <img src="/image/Zoom.png" alt="" class="w-auto h-20">
+                    <p class="text-center text-navy-bg text-3xl font-medium text-nowrap">Click here to Join!</p>
+                </div>
+            </a>
+        </div>
+
+    </div>
 </x-layout>

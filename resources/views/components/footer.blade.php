3fca0c4f71d3c799ffb9dab7a1ce10aa20fd0dd8
Muhammad-Rizki-Putra <muhammad22517@mail.unpad.ac.id>
2024-08-18 17:53:29 +0700

    penyelesaian footer mobile

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
 <div class="hidden md:flex flex-row items-start justify-center w-full h-auto bg-cream-bg space-x-64  p-5">
     <div class="flex flex-col items-center justify-center">
-        <h1 class="text-xl pb-3 text-nowrap font-bold">Brought To You By</h1>
+        <h1 class="text-xl pb-2 text-nowrap font-bold">Brought To You By</h1>
         <div class="flex flex-row items-center justify-center">
             <img src="/image/logo_ifest4.png" alt="" class="h-auto w-auto pr-3">
             <img src="/image/logo_himatif.png" alt="" class="h-auto w-auto">
@@ -8,11 +8,11 @@
     </div>
 
     <div class="flex flex-col items-center justify-center">
-        <h1 class="text-xl pb-3 text-nowrap font-bold">Partner Links</h1>
+        <h1 class="text-xl pb-2 text-nowrap font-bold">Partner Links</h1>
     </div>
 
     <div class="flex flex-col items-center justify-start">
-        <h1 class="text-xl pb-3 font-bold">Contacts</h1>
+        <h1 class="text-xl pb-2 font-bold">Contacts</h1>
         <div>
             <div class="flex flex-row">
                 <img src="/image/Email.png" alt="logo-email" class="mr-2 object-none">
@@ -27,7 +27,7 @@
     </div>
 
     <div class="flex flex-col items-center justify-center">
-        <h1 class="text-xl pb-3 text-nowrap font-bold">Social Media</h1>
+        <h1 class="text-xl pb-2 text-nowrap font-bold">Social Media</h1>
         <div class="flex space-x-2">
             <a href="https://www.instagram.com/ifestunpad/"><img src="/image/Instagram.png" alt=""></a>
             <a href="https://www.tiktok.com/@ifestunpad?lang=en"><img src="/image/Tiktok.png" alt=""></a>
@@ -39,41 +39,43 @@
 
 <!-- mobile ver -->
 
-<div class="md:hidden flex flex-row items-start justify-center w-screen h-auto bg-cream-bg space-x-64  p-5">
-    <div class="flex flex-col items-center justify-center">
-        <h1 class="text-xl pb-3 text-nowrap font-bold">Brought To You By</h1>
-        <div class="flex flex-row items-center justify-center">
-            <img src="/image/logo_ifest4.png" alt="" class="h-auto w-auto pr-3">
+<div class="md:hidden grid grid-flow-row-dense grid-cols-2 grid-rows-2 w-screen h-auto bg-cream-bg px-5">
+    <div class="flex flex-col items-center justify-start pt-3">
+        <h1 class="text-sm text-nowrap font-bold pb-2">Brought To You By</h1>
+        <div class="flex flex-row items-center justify-center w-[30%]">
+            <img src="/image/logo_ifest4.png" alt="" class="h-auto w-auto">
             <img src="/image/logo_himatif.png" alt="" class="h-auto w-auto">
         </div>
     </div>
 
-    <div class="flex flex-col items-center justify-center">
-        <h1 class="text-xl pb-3 text-nowrap font-bold">Partner Links</h1>
+    <div class="flex flex-col items-center justify-start pt-3">
+        <h1 class="text-sm text-nowrap font-bold">Partner Links</h1>
     </div>
 
-    <div class="flex flex-col items-center justify-start">
-        <h1 class="text-xl pb-3 font-bold">Contacts</h1>
+    <div class="flex flex-col items-center justify-start pt-3">
+        <h1 class="text-sm font-bold pb-2">Contacts</h1>
         <div>
             <div class="flex flex-row">
                 <img src="/image/Email.png" alt="logo-email" class="mr-2 object-none">
-                <p>marketing.ifest@gmail.com</p>
+                <p class="text-xs">marketing.ifest@gmail.com</p>
             </div>
 
             <div class="flex flex-row">
                 <img src="/image/Telephone.png" alt="logo-telpon" class="mr-2 object-none">
-                <p>Rafa (082261552004)</p>
+                <p class="text-xs">Rafa (082261552004)</p>
             </div>
         </div>
     </div>
 
-    <div class="flex flex-col items-center justify-center">
-        <h1 class="text-xl pb-3 text-nowrap font-bold">Social Media</h1>
-        <div class="flex space-x-2">
+    <div class="flex flex-col items-center justify-start pt-3">
+        <h1 class="text-sm pb-2 text-nowrap font-bold">Social Media</h1>
+        <div class="flex space-x-2 w-[70%]">
             <a href="https://www.instagram.com/ifestunpad/"><img src="/image/Instagram.png" alt=""></a>
             <a href="https://www.tiktok.com/@ifestunpad?lang=en"><img src="/image/Tiktok.png" alt=""></a>
             <a href="https://www.youtube.com/@informaticsfestivalhimatif4954"><img src="/image/Youtube.png" alt=""></a>
             <a href="https://www.linkedin.com/company/ifest-unpad/"><img src="/image/Linkedin.png" alt=""></a>
         </div>
     </div>
+
+
 </div>

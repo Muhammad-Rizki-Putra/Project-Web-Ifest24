4b6217e672c21e88b41a5006a02bdc14474565fe
Jason Natanael <89887196+JasonMs17@users.noreply.github.com>
2024-08-24 15:37:09 +0700

    update login, register, profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -6,12 +6,12 @@
             <div class="grid grid-cols-1 h-46 bg-cream-bg place-content-center p-4 rounded-lg">
                 <div class="grid grid-cols-3 grid-rows-2 gap-4">
                     <div class="text-left flex-col truncate">
-                        <p class="font-bold">Name</p>
-                        <p>{{ $user->name }}</p>
-                    </div>
-                    <div class="text-left flex-col truncate">
                         <p class="font-bold">Full Name</p>
                         <p>{{ $user->fullname }}</p>
+                    </div>
+                    <div class="text-left flex-col truncate">
+                        <p class="font-bold">Email</p>
+                        <p>{{ $user->email }}</p>
                     </div>
                     <div class="text-left flex flex-col truncate">
                         <div class="flex flex-row items-center gap-2">
@@ -21,15 +21,12 @@
                             </a>
                         </div>
                         <div class="flex flex-row items-center gap-2">
-                            <button class="flex items-center gap-2">
+                            <a href="/logout" class="flex items-center gap-2">
                                 <img src="/image/profile_logout.png" alt="" class="profile-buttons">
                                 Logout
-                            </button>
+                            </a>
                         </div>
-                    </div>
-                    <div class="text-left flex-col truncate">
-                        <p class="font-bold">Email</p>
-                        <p>{{ $user->email }}</p>
+
                     </div>
                     <div class="text-left flex-col truncate">
                         <p class="font-bold">Birthday</p>
@@ -39,32 +36,36 @@
                         <p class="font-bold">Education Level</p>
                         <p>{{ $user->education_level }}</p>
                     </div>
-                </div>
+                    {{-- <div class="text-left flex-col truncate">
+                        <p class="font-bold">Name</p>
+                        <p>{{ $user->name }}</p>
+                </div> --}}
             </div>
         </div>
+    </div>
 
-        <div class="register-location p-8">
-            <div class="text-2xl profile-section-name">Registered At</div>
-            <div class="bg-cream-bg p-4 rounded-lg flex items-center justify-between">   
-                <div>
-                    <p class="font-bold text-xl mb-2">Name</p>
-                    <div class="flex flex-col items-start gap-2">
-                        <div class="flex items-center text-sm gap-2">
-                            <img src="/image/register_date.png" alt="" class="profile-buttons">
-                            <p>Day, dd - mm - yyyy</p>
-                        </div>
+    <div class="register-location p-8">
+        <div class="text-2xl profile-section-name">Registered At</div>
+        <div class="bg-cream-bg p-4 rounded-lg flex items-center justify-between">
+            <div>
+                <p class="font-bold text-xl mb-2">Name</p>
+                <div class="flex flex-col items-start gap-2">
+                    <div class="flex items-center text-sm gap-2">
+                        <img src="/image/register_date.png" alt="" class="profile-buttons">
+                        <p>Day, dd - mm - yyyy</p>
+                    </div>
 
-                        <div class="flex items-center text-sm gap-2">
-                            <img src="/image/register_location.png" alt="" class="profile-buttons">
-                            <p>Location</p>
-                        </div>
+                    <div class="flex items-center text-sm gap-2">
+                        <img src="/image/register_location.png" alt="" class="profile-buttons">
+                        <p>Location</p>
                     </div>
                 </div>
+            </div>
 
-                <div class="self-end text-xs">
-                    <button class="bg-sky-900 text-white px-2 py-1 w-20 rounded-lg">Details</button>
-                </div>
+            <div class="self-end text-xs">
+                <button class="bg-sky-900 text-white px-2 py-1 w-20 rounded-lg">Details</button>
             </div>
         </div>
+    </div>
     </div>
 </x-layout>
